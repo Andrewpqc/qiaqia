3ac93e1f6f8c6297985a6df6630807855f714bae
Andrewpqc <m13636038496@163.com>
2018-11-22 23:02:30 +0800

    rm csapp

--- server/server.hpp
+++ server/server.hpp
@@ -1,5 +1,5 @@
-#ifndef _CHATROOM_SERVER_H_
-#define _CHATROOM_SERVER_H_
+#ifndef _CHATROOM_SERVER_HPP_
+#define _CHATROOM_SERVER_HPP_
 
 #include <string>
 #include <map>
@@ -20,11 +20,48 @@
 
 #include "../utils/error_functions.h"
 
+/* 监听缓冲队列大小 */
 #define LISTENQ 50
+
+/* 内核epoll表的初始大小 */
 #define EPOLL_SIZE 500
+
 #define MAXLINE 8192
 
+/* 当系统中只要一个用户的时候给出提示消息 */
+#define CAUTION "\033[31mSYSTEM MESSAGE:\033[0mOnly you in the chat room now!"
+
+/* 新用户登录后的欢迎信息 */
+#define SERVER_WELCOME "\033[31mSYSTEM MESSAGE:\033[0m[%s] joined to the chat room,welcome!"
+
+/* 当某一用户离开时的通知信息 */
+#define LEAVE_INFO "\033[31mSYSTEM MESSAGE:\033[0m[%s] leaved the chat room!"
+
+/* 其他用户收到消息的前缀 */
+#define SERVER_MESSAGE "[%s] say >> %s"
+
+/* 表示客户端地址的字符串的最大长度 */
+#define ADDRSTRLENGTH  (NI_MAXHOST + NI_MAXSERV +10)
+
+
 namespace server_ns {
+
+    void trim(const char *strIn, char *strOut) {
+
+        size_t i, j;
+
+        i = 0;
+
+        j = strlen(strIn) - 1;
+
+        while (strIn[i] == ' ')
+            ++i;
+
+        while (strIn[j] == ' ')
+            --j;
+        strncpy(strOut, strIn + i, j - i + 1);
+        strOut[j - i + 1] = '\0';
+    }
 
     typedef struct {
         std::string clientIP;
@@ -47,11 +84,33 @@ namespace server_ns {
 
 
         int broadcast(int sender_fd, char *msg, int recv_len) {
-
+            for (auto it: this->clients) {
+                if (it.first != sender_fd) {
+                    if (send(it.first, msg, MAXLINE, 0) < 0) {
+                        return -1;
+                    }
+                }
+            }
+            return recv_len;
         }
 
         int show_userinfo_to_client(int connfd, int len) {
+            char message[MAXLINE];
+            sprintf(message, "\033[32mHere are %lu users online now!", this->clients.size());
+            sprintf(message, "%s\nHOST        PORT    JOIN_TIME                  USERNAME", message);
+            for (auto client:this->clients) {
 
+                sprintf(message, "%s\n%s   %s   %s   %s", message,
+                        client.second.clientIP.c_str(),
+                        client.second.clientPort.c_str(),
+                        client.second.joinAt,
+                        client.second.clientNickname.c_str());
+            }
+            sprintf(message, "%s\033[0m", message);
+            if (send(connfd, message, strlen(message), 0) < 0) {
+                return -1;
+            }
+            return len;
         }
 
         int get_listen_fd(const std::string &port) {
@@ -159,6 +218,95 @@ namespace server_ns {
             //give a output here
         }
 
+        int get_msg_and_forward_to_clients(int connfd) {
+            // buf[MAXLINE] 接收新消息
+            // message[MAXLINE] 保存格式化的消息
+            char buf[MAXLINE], message[MAXLINE];
+
+            bzero(buf, MAXLINE);
+            bzero(message, MAXLINE);
+
+            // recv new msg
+            std::cout << "read from client(clientID = " << connfd << ")" << std::endl;
+            ssize_t len = recv(connfd, buf, MAXLINE, 0);
+
+
+            // set the current user's nickname,this will be
+            // run for the first msg for every client
+            if (!this->clients[connfd].isNicknameSet) {
+                this->clients[connfd].clientNickname = static_cast<std::string>(buf);
+                this->clients[connfd].isNicknameSet = true;
+
+                //broadcast the welcome message to all other users
+                sprintf(message, SERVER_WELCOME, this->clients[connfd].clientNickname.c_str());
+                return this->broadcast(connfd, message, len);
+
+            }
+
+            // if the client close the connection
+            if (len == 0) {
+                //close the server side connfd
+                if (close(connfd) != 0) errExit("error close fd");
+
+                //remove the client_info from the `clients` set
+                this->clients.erase(connfd);
+
+                //print log to the server side stdout
+                std::cout << "ClientID = " << connfd
+                          << " closed.\nnow there are "
+                          << this->clients.size()
+                          << " client in the chat room"
+                          << std::endl;
+
+                // broadcast the leave info
+                sprintf(message, LEAVE_INFO, this->clients[connfd].clientNickname.c_str());
+                return this->broadcast(connfd, message, len);
+            } else {
+                // if there only one user in the chat room,
+                // send caution message
+                if (this->clients.size() == 1) {
+                    if (send(connfd, CAUTION, strlen(CAUTION), 0) < 0) {
+                        return -1;
+                    }
+                }
+
+
+                if (strncasecmp(buf, "$ show users", strlen("$ show users")) == 0) {
+                    return this->show_userinfo_to_client(connfd, len);
+                } else if (buf[0] == '>') {
+                    std::string command = static_cast<std::string>(buf);
+                    std::size_t pos1 = command.find_first_of(' ');
+                    std::size_t pos2 = command.find_last_of(' ');
+
+                    //提取出name,msg
+                    std::string name = command.substr(pos1 + 1, pos2 - 1);
+                    std::string msg = command.substr(pos2 + 1);
+
+                    //去掉可能的空格
+                    char name_c[name.size()], msg_c[msg.size()];
+                    trim(name.c_str(), name_c);
+                    trim(msg.c_str(), msg_c);
+
+                    //转发给要求的人
+                    for (auto client: this->clients) {
+                        if (client.second.clientNickname == (static_cast<std::string>(name_c))) {
+                            if (send(client.first, msg_c, strlen(msg_c), 0) < 0) {
+                                return -1;
+                            }
+                        }
+                    }
+                    return len;
+                }
+
+                // format the msg to be send to clients
+                sprintf(message, SERVER_MESSAGE, this->clients[connfd].clientNickname.c_str(), buf);
+
+                // broadcast
+                return this->broadcast(connfd, message, len);
+
+            }
+        }
+
 
     public:
         std::map<int, ClientInfo> clients;
@@ -174,14 +322,13 @@ namespace server_ns {
             if (close(this->epollFd) == -1) errExit("close epollFd");
         }
 
-
         void start() {
             create_listener();
 
             socklen_t client_addr_len;
             struct sockaddr_storage client_addr;
 
-#define ADDRSTRLENGTH  (NI_MAXHOST + NI_MAXSERV +10)
+
             char hostname[NI_MAXHOST], port[NI_MAXSERV], addr_str[ADDRSTRLENGTH];
 
             static struct epoll_event events[EPOLL_SIZE];
@@ -236,97 +383,8 @@ namespace server_ns {
                     }
                 }
             }
-
         }
 
-        int get_msg_and_forward_to_clients(int connfd) {
-            // buf[MAXLINE] 接收新消息
-            // message[MAXLINE] 保存格式化的消息
-            char buf[MAXLINE], message[MAXLINE];
-
-            bzero(buf, MAXLINE);
-            bzero(message, MAXLINE);
-
-            // recv new msg
-            std::cout << "read from client(clientID = " << connfd << ")" << std::endl;
-            ssize_t len = recv(connfd, buf, MAXLINE, 0);
-
-
-            // set the current user's nickname,this will be
-            // run for the first msg for every client
-            if (!this->clients[connfd].isNicknameSet) {
-                this->clients[connfd].clientNickname = static_cast<std::string>(buf);
-                this->clients[connfd].isNicknameSet = true;
-
-                //broadcast the welcome message to all other users
-                sprintf(message, SERVER_WELCOME, this->clients[connfd].clientNickname.c_str());
-                return this->broadcast(connfd, message, len);
-
-            }
-
-            // if the client close the connection
-            if (len == 0) {
-                //close the server side connfd
-                if(close(connfd)!=0);
-
-                //remove the client_info from the `clients` set
-                this->clients.erase(connfd);
-
-                //print log to the server side stdout
-                std::cout << "ClientID = " << connfd
-                          << " closed.\nnow there are "
-                          << this->clients.size()
-                          << " client in the chat room"
-                          << std::endl;
-
-                // broadcast the leave info
-                sprintf(message, LEAVE_INFO, this->clients[connfd].client_nickname.c_str());
-                return this->broadcast(connfd, message, len);
-            } else {
-                // if there only one user in the chat room,
-                // send caution message
-                if (this->clients.size() == 1) {
-                    if (send(connfd, CAUTION, strlen(CAUTION), 0) < 0) {
-                        return -1;
-                    }
-                }
-
-
-                if (strncasecmp(buf, "$ show users", strlen("$ show users")) == 0) {
-                    return this->show_userinfo_to_client(connfd, len);
-                } else if (buf[0] == '>') {
-                    std::string command = static_cast<std::string>(buf);
-                    std::size_t pos1 = command.find_first_of(' ');
-                    std::size_t pos2 = command.find_last_of(' ');
-
-                    //提取出name,msg
-                    std::string name = command.substr(pos1 + 1, pos2 - 1);
-                    std::string msg = command.substr(pos2 + 1);
-
-                    //去掉可能的空格
-                    char name_c[name.size()], msg_c[msg.size()];
-                    trim(name.c_str(), name_c);
-                    trim(msg.c_str(), msg_c);
-
-                    //转发给要求的人
-                    for (auto it = this->clients.begin(); it != this->clients.end(); it++) {
-                        if (it->second.client_nickname == (static_cast<std::string>(name_c))) {
-                            if (send(it->first, msg_c, strlen(msg_c), 0) < 0) {
-                                return -1;
-                            }
-                        }
-                    }
-                    return len;
-                }
-
-                // format the msg to be send to clients
-                sprintf(message, SERVER_MESSAGE, this->clients[connfd].client_nickname.c_str(), buf);
-
-                // broadcast
-                return this->broadcast(connfd, message, len);
-
-        }
-    }; //class Server
-
-} // namespace server_ns
+    }; // class Server
+    } // namespace server_ns
 #endif
--- server/servermain.cpp
+++ server/servermain.cpp
@@ -7,15 +7,15 @@
 #include "../utils/cmdline.h"
 
 int main(int argc, char **argv) {
-    //command parser
+    /*command parser*/
     cmdline::parser cmd;
     cmd.add<std::string>("port", 'p', "port number to be listened, default 8080", false, "8080");
     cmd.parse_check(argc, argv);
 
-    //get port from the command parser
+
     std::string port = cmd.get<std::string>("port");
 
-    //this server instance
+    //create server instance
     server_ns::Server server(port);
 
     //start the event loop
